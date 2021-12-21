75414094b76b4e1c69e0908ccb3e24bc1753797c
AndreyMatykhin <matykhinAnd2021@gmail.com>
2021-12-21 20:07:52 +0300

    My_DZ_5

--- Matyukhin_Andrey_dz_5/task_5_5.py
+++ Matyukhin_Andrey_dz_5/task_5_5.py
@@ -1,11 +1,11 @@
-src = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
+src = [2, 2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
 rep = set()
 no_rep = set()
 for el in src:
     if el in no_rep:
         rep.add(el)
         no_rep.remove(el)
-    else:
+    elif not(el in rep):
         no_rep.add(el)
 result = [num for num in src if num in no_rep]
 print(result)
